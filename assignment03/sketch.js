4b925fbe2bfd07e24cca2ee7b73653c8a1e3c1ee
DWH52 <donniehunter52@gmail.com>
2024-01-25 13:21:07 -0600

    Updating display dimensions.

--- assignment03/sketch.js
+++ assignment03/sketch.js
@@ -18,7 +18,7 @@ function preload()
 
 function setup() 
 {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(width, height);
   imageMode(CENTER);
   // Random coordinates with constraints so that the sprites do not
   // load off the display area.
