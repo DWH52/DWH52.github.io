c81a14b8dabad569e118fd2c9b45895aec16946a
DWH52 <donniehunter52@gmail.com>
2024-02-22 14:17:53 -0600

    adding more sound files for use

--- assignment05/sketch.js
+++ assignment05/sketch.js
@@ -1,4 +1,4 @@
-const synth = new Tone.Synth().toDestination();
+//const synth = new Tone.Synth().toDestination();
 
 function preload()
 {
@@ -8,6 +8,9 @@ function preload()
       base : "assets/sounds/baseDrum.mp3",
       eSnare: "assets/sounds/electricSnare.mp3",
       tSnare: "assets/sounds/tightSnare.mp3",
+      hz500: "assets/sounds/500hz.mp3",
+      hz600: "assets/sounds/600hz.mp3",
+      hz700: "assets/sounds/700hz.mp3",
     }
   ).toDestination();
 }
@@ -17,20 +20,33 @@ function setup()
   createCanvas(400, 400);
 
   button1 = createButton('Rock');
-  button1.position(75, 100);
+  button1.position(75, 25);
   button1.mousePressed(() => soundFX.player('rock').start());
-
+/*
   button2 = createButton('Base Drum');
-  button2.position(75, 150);
+  button2.position(75, 50);
   button2.mousePressed(()=> soundFX.player('base').start());
 
   button3 = createButton('Electronic Snare');
-  button3.position(75,200);
+  button3.position(75,75);
   button3.mousePressed(() => soundFX.player('eSnare').start());
 
   button4 = createButton('Tight Snare');
-  button4.position(75,250);
+  button4.position(75,100);
   button4.mousePressed(() => soundFX.player('tSnare').start());
+
+  button5 = createButton('500hz');
+  button5.position(75,125);
+  button5.mousePressed(() => soundFX.player('hz500').start());
+
+  button6 = createButton('600hz');
+  button6.position(75,150);
+  button6.mousePressed(() => soundFX.player('hz600').start());
+
+  button7 = createButton('700hz');
+  button7.position(75,175);
+  button7.mousePressed(() => soundFX.player('hz700').start());
+  */
 }
 
 function draw()
