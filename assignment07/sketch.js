36b868533aba60053ea13d7c174a44debeebb30f
DWH52 <donniehunter52@gmail.com>
2024-03-09 10:39:53 -0600

    Finalizing assignment07

--- assignment07/sketch.js
+++ assignment07/sketch.js
@@ -1,4 +1,50 @@
-let chaChing = new Tone.PolySynth(Tone.AMSynth).toDestination();
+/*
+BACKUP IF I CANNOT GET MODULATION TO WORK HOW I WANT IT TO.
+let coinSFX1 = new Tone.Synth();
+coinSFX1.volume.value = -10;
+let coinSFX2 = new Tone.Synth();
+coinSFX2.volume.value = -10;
+const bitCrush = new Tone.BitCrusher(16).toDestination();
+bitCrush.wet = 1;
+const coinDelay = new Tone.Delay("16n")
+coinSFX1.connect(bitCrush).toDestination();
+coinSFX2.connect(coinDelay);
+coinDelay.connect(bitCrush).toDestination();
+*/
+
+const squareWave1 = new Tone.Synth({
+  oscillator: {
+    type: "square"
+  },
+  envelope: {
+    attack : .01,
+    decay : 0.2,
+    sustain : 0.5,
+    release : 0.2,
+  },
+  volume : -15,
+});
+
+const squareWave2 = new Tone.Synth({
+  oscillator: {
+    type: "square"
+  },
+  envelope: {
+    attack : .01,
+    decay : 0.5,
+    sustain : 0.5,
+    release : 0.5,
+  },
+  volume : -15,
+});
+
+const bitCrush = new Tone.BitCrusher(16);
+bitCrush.wet = .75;
+const coinDelay = new Tone.Delay("16n");
+squareWave1.chain(bitCrush, Tone.Destination);
+squareWave2.chain(bitCrush, coinDelay, Tone.Destination);
+
+
 //The way to create the Super Mario coin sound is by 
 //playing an octave or 4th interval upwards at a fast 
 //tempo. Use a square wave from a synthesizer and 
@@ -13,7 +59,6 @@ function preload()
 
 function setup() 
 {
-  Tone.start();
   createCanvas(400, 400);
   imageMode(CENTER);
   
@@ -30,5 +75,8 @@ function draw()
 
 function mousePressed()
 {
-  chaChing.triggerAttackRelease(["A2","D3"], .5);
+  //coinSFX1.triggerAttackRelease("B5", "16n");
+  //coinSFX2.triggerAttackRelease("E6", "8n");
+  squareWave1.triggerAttackRelease("B5", "16n");
+  squareWave2.triggerAttackRelease("E6", "8n");
 }
